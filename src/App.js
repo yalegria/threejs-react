e235544c325232cef550e843e5b77bde4bffa407
yuriaru <iguanadesignsnet@gmail.com>
2022-07-20 14:43:07 -0400

    mounting the component correctly

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+
+import { useEffect, useRef } from "react";
 import * as THREE from 'three';
 
-function App() {
+const App = () => {
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const mountRef = useRef(null);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  useEffect( () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
 
-  const geometry = new THREE.BoxGeometry(1,1,1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  camera.position.z = 5;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mountRef.current.appendChild(renderer.domElement);
 
-  function animate() {
-    requestAnimationFrame (animate);
+    const geometry = new THREE.BoxGeometry(1,1,1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
+    scene.add(cube);
+    camera.position.z = 5;
 
-  animate();
+    function animate() {
+      requestAnimationFrame (animate);
 
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    }
+
+    animate();
+
+    return () => mountRef.current.removeChild(renderer.domElement);
+  }, []);
+  
+  
   return (
-    <div className="App">
+    <div ref={mountRef} className="App">
       
-    
-      
-    
     </div>
   );
+  
 }
 
 export default App;
